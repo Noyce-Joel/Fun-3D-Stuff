dd90849bccd5e31dfaa1c565f298d0797d5639ab
Joel Noyce <noyce.joel@gmail.com>
2023-05-23 02:58:52 +0100

    new site2

--- components/Page.js
+++ components/Page.js
@@ -6,8 +6,10 @@ import {
   Scroll,
   useScroll,
   Image as ImageImpl,
+  Text,
+  Html
 } from "@react-three/drei";
-import { useSpring, animated, config, useSpringRef } from "react-spring";
+import { useSpring, animated, useInView} from "react-spring";
 
 function Image({ ...props }) {
   const ref = useRef();
@@ -27,45 +29,69 @@ function Images() {
     group.current.children[4].material.zoom = 1 + data.range(0, 1 / 2) / 4;
     group.current.children[4].material.zoom = 1 + data.range(0, 1 / 2) / 4;
   });
+  const fontProps = { fontSize: 0.2, letterSpacing: -0.05, color: 'white', lineHeight: 1
+  }
+  const [ref, springs] = useInView(
+    () => ({
+      from: {
+        scale: 0.55,
+      },
+      to: {
+        scale: 1,
+      },
+      config: { tension: 210, friction: 100, mass: 25 },
+    }),
+    {
+      rootMargin: "-27% 0%",
+    }
+  );
   return (
     <group ref={group}>
       <Image
+
         rotation={[0, 0.4, 0]}
-        position={[0, 0, -1]}
+        position={[-1, 0, -1]}
         scale={4.5}
         url="Img3.jpg"
       />
-      <Image position={[10, 0, -5]} scale={4.5} url="Img2.jpg" />
+      <Image position={[9, 0, -5]} scale={4.5} url="Img2.jpg" />
       <Image
         rotation={[0, -0.4, 0]}
-        position={[20, 0, -1.5]}
+        position={[19, 0, -1.5]}
         scale={4.5}
         url="Img6.jpg"
       />
-      <Image position={[5, 0, -3]} scale={4.5} url="Img4.jpg" />
-      <Image position={[15, 0, -3]} scale={4.5} url="Img5.jpg" />
+      <Image position={[4, 0, -3]} scale={4.5} url="Img4.jpg" />
+      <Image position={[14, 0, -3]} scale={4.5} url="Img5.jpg" />
+      <Text anchorX={-21.7} {...fontProps}> Hey, I'm Rosie and I've been an illustrator and </Text>
+      <Text anchorX={-21.7} anchorY={.2} {...fontProps}> artist for the past 15 years.</Text>
+      <Text anchorX={-51.7} {...fontProps}>These are some of the projects I've worked on. </Text>
+      <Text anchorX={-51.7} anchorY={.2} {...fontProps}>I have a passion for drawing and painting. </Text>
+      <Text anchorX={-height / 1.7} anchorY={2.5} {...fontProps}>PORTFOLIO</Text>
+     
       <Image
-        rotation={[0, 0.2, 0]}
-        position={[25, 0, -1]}
+        rotation={[0, 0, 0]}
+        position={[49, 0, -0.5]}
         scale={4.5}
-        url="Img7.jpg"
+        url="Img8.jpg"
       />
-    <Image position={[35, 0, -5]} scale={4.5} url="Img8.jpg" />
+    <Image position={[34, 0, -5]} scale={4.5} url="Img7.jpg" />
     <Image
         rotation={[0, -0.4, 0]}
-        position={[45, 0, -1.5]}
+        position={[44, 0, -1.5]}
         scale={4.5}
         url="Img9.jpg"
       />
-      <Image position={[30, 0, -3]} scale={4.5} url="Img4.jpg" />
-      <Image position={[40, 0, -3]} scale={4.5} url="Img5.jpg" />
+      <Image position={[29, 0, -3]} scale={4.5} url="Img4.jpg" />
+      <Image position={[39, 0, -3]} scale={4.5} url="Img5.jpg" />
+      
     </group>
   );
 }
 
 export default function Page() {
   const spring = useSpring({
-    config: { stiffness: 15, friction: 90, damping: 5, mass: 0.5 },
+    config: { stiffness: 15, friction: 90, damping: 5, mass: 1.5 },
     from: {
       x: -150,
       y: 500,
@@ -81,7 +107,7 @@ export default function Page() {
     }
   });
   const line = useSpring({
-    config: { stiffness: 15, friction: 170, damping: 42, mass: 2 },
+    config: { stiffness: 15, friction: 170, damping: 32, mass: 2 },
     from: {
       x: 1700
     },
@@ -92,9 +118,10 @@ export default function Page() {
   return (
     <Canvas gl={{ antialiasing: false }} dpr={[1, 2]}>
       <Suspense fallback={null}>
-        <ScrollControls horizontal={true} distance={1} pages={10}>
+        <ScrollControls horizontal={true} damping={0.4} pages={17}>
           <Scroll>
             <Images />
+            
           </Scroll>
           <Scroll html>
             <div className="overlay">
@@ -106,6 +133,7 @@ export default function Page() {
                   <animated.div className="line" style={{ ...line }} />
                 </div>
               </div>
+             
             </div>
           </Scroll>
         </ScrollControls>
---
+++ components/Text.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Text() {
+  return (
+    <div className='about'>Hey, I'm Rosie and I've been an artist and illustrator for the past 15 years. I"m extremely cute. But shh!</div>
+  )
+}
+
+export default Text
